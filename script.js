de9455c8c17e0327f56904a6fff9d674825313b0
drpishgaman <dr.pishgaman@gmail.com>
2025-04-14 19:30:15 +0330

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ function login() {
   const correctPassword = "1234";
 
   if (username === correctUsername && password === correctPassword) {
-    alert("ورود موفقیت‌آمیز بود!");
-    // در آینده: انتقال به صفحه اصلی
+    window.location.href = "home.html"; // انتقال به صفحه اصلی
   } else {
     document.getElementById("error").textContent = "نام کاربری یا رمز عبور اشتباه است.";
   }
